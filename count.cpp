d60446c9afd5ee9221fa4174c5853a76a988e84c
Tom Hartley <tomhartley007@gmail.com>
2025-07-29 16:37:59 +0930

    Show total even values

--- count.cpp
+++ count.cpp
@@ -4,7 +4,7 @@ int count(int array[], int n) {
     int sum = 0;
     for (int i=0; i<n; i++) {
         if (array[i]%2==0) {
-            sum = sum+array[i];
+            sum++;
         }
     }
     return sum;
