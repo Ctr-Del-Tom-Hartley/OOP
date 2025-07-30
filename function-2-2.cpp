ead86a61a1d0931e3ae1d5955e59c19b4596ef4b
Tom Hartley <tomhartley007@gmail.com>
2025-07-30 16:43:09 +0930

    Uploading all 'medium problems'

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,10 +1,13 @@
-double average(int array[], int n) {
-    int sum=0;
-    for (int i=0; i<n; i++) {
-        sum=sum+array[i];
+int max_element(int array[], int n) {
+    if (n<0) {
+        return 0;
     }
-
-    double average=(double)sum/n;
-
-    return average;
+    int max=array[0];
+    
+    for (int i=1; i<n; i++) {
+        if (array[i]>max) {
+            max=array[i];
+        }
+    }
+    return max;
 }
---
+++ function-2-4.cpp
@@ -0,0 +1,12 @@
+bool is_ascending(int array[], int n) {
+    bool is_ascending=true;
+    for (int i=1; i<n; i++) {
+        if (array[i]>=array[i-1]) {
+            is_ascending=true;
+        } else {
+            is_ascending=false;
+            break;
+        }
+    }
+    return is_ascending;
+}
