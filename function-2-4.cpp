c755dbfaf914cf477ed48ccf24f9191dbcef9af1
Tom Hartley <tomhartley007@gmail.com>
2025-07-31 13:19:38 +0930

    Uploading all 'tricky problems'

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -1,9 +1,7 @@
 bool is_ascending(int array[], int n) {
     bool is_ascending=true;
     for (int i=1; i<n; i++) {
-        if (array[i]>=array[i-1]) {
-            is_ascending=true;
-        } else {
+        if (array[i]<=array[i-1]) {
             is_ascending=false;
             break;
         }
